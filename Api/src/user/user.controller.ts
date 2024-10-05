c302097630c217012285760e938eb5ff5347f7cc
Guilherme Bittencourt <guilhermebittencourtcorrea@gmail.com>
2024-10-05 16:45:32 -0300

    Atualizado função de listagem de users

--- Api/src/user/user.controller.ts
+++ Api/src/user/user.controller.ts
@@ -1,13 +1,23 @@
-import { Controller, Delete, Param, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Param, Delete, NotFoundException } from '@nestjs/common';
 import { UserService } from './user.service'; // Ajuste o caminho conforme necessário
+import { User } from './user.entity'; // Ajuste o caminho conforme necessário
 
 @Controller('users') // Use 'users' como plural para a rota
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Get() // Rota para obter todos os usuários
+  async getAllUsers(): Promise<User[]> {
+    return this.userService.getAllUsers(); // Chama o método do serviço
+  }
+
+  @Get(':id') // Rota para obter um usuário pelo ID
+  async getUser(@Param('id') id: number): Promise<User> {
+    return this.userService.getUserById(id); // Chama o método do serviço
+  }
+
   @Delete(':id') // Rota para deletar um usuário pelo ID
-  @HttpCode(HttpStatus.NO_CONTENT) // Define o status 204 para sucesso sem conteúdo
   async deleteUser(@Param('id') id: string): Promise<void> {
-    await this.userService.deleteUser(id); // Chama o método de deletar do serviço
+    await this.userService.deleteUser(id);
   }
 }
--- Api/src/user/user.service.ts
+++ Api/src/user/user.service.ts
@@ -1,28 +1,32 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { User } from './user.entity';
-import * as bcrypt from 'bcrypt';
+import { User } from './user.entity'; // Ajuste o caminho conforme necessário
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
-  ) { }
+  ) {}
 
-  async findByUsername(username: string): Promise<User> {
-    return this.userRepository.findOne({ where: { username } });
-  }
   async deleteUser(id: string): Promise<void> {
     const result = await this.userRepository.delete(id);
     if (result.affected === 0) {
       throw new NotFoundException('Usuário não encontrado');
     }
   }
-  async create(username: string, password: string): Promise<User> {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = this.userRepository.create({ username, password: hashedPassword });
-    return this.userRepository.save(newUser);
+
+  async getUserById(id: number): Promise<User> {
+    const user = await this.userRepository.findOne({ where: { id } });
+    if (!user) {
+      throw new NotFoundException('Usuário não encontrado');
+    }
+    return user;
+  }
+
+  // Novo método para listar todos os usuários
+  async getAllUsers(): Promise<User[]> {
+    return this.userRepository.find(); // Retorna todos os usuários
   }
 }
